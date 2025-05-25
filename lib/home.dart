32d6c649967d013afc1c4e8f9b7a39b95c07ceb5
Synthever <rakha@students.amikom.ac.id>
2025-05-25 19:37:42 +0700

    Up
    Up

---
+++ lib/home.dart
@@ -0,0 +1,230 @@
+import 'package:appjadwal/view.dart';
+import 'package:flutter/material.dart';
+import 'package:appjadwal/senin.dart';
+import 'package:appjadwal/selasa.dart';
+import 'package:appjadwal/carousel.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+final List<String> imglist = [
+  'https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?w=1380&t=st=1699238412~exp=1699239012~hmac=75e2d00d2b55b34ba02ed6cc46fe9ea8d8a0aeb7857db3e101c2e1d6a3b10037'
+];
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.cyan,
+      appBar: AppBar(
+          backgroundColor: Colors.grey[900],
+          title: const Text('Dashboard'),
+          titleTextStyle: const TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          )),
+      body: GridView.count(
+        padding: const EdgeInsets.all(25),
+        crossAxisCount: 2,
+        children: <Widget>[
+          Card(
+            margin: const EdgeInsets.all(8),
+            child: InkWell(
+              onTap: () {
+                Navigator.pushReplacement(
+                    context, MaterialPageRoute(builder: (context) => const Senin()));
+              },
+              borderRadius: BorderRadius.circular(4),
+              splashColor: Colors.black,
+              child: const Center(
+                child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Icon(
+                        Icons.school,
+                        size: 70,
+                        color: Colors.cyan,
+                      ),
+                      Text(
+                        "Senin",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontFamily: 'Montserrat',
+                        ),
+                      ),
+                    ]),
+              ),
+            ),
+          ),
+          Card(
+            margin: const EdgeInsets.all(8),
+            child: InkWell(
+              onTap: () {
+                Navigator.pushReplacement(
+                    context, MaterialPageRoute(builder: (context) => const Selasa()));
+              },
+              splashColor: Colors.black,
+              child: const Center(
+                child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Icon(
+                        Icons.school,
+                        size: 70,
+                        color: Colors.cyan,
+                      ),
+                      Text(
+                        "Selasa",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontFamily: 'Montserrat',
+                        ),
+                      ),
+                    ]),
+              ),
+            ),
+          ),
+          Card(
+            margin: const EdgeInsets.all(8),
+            child: InkWell(
+              onTap: () {},
+              splashColor: Colors.black,
+              child: const Center(
+                child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Icon(
+                        Icons.school,
+                        size: 70,
+                        color: Colors.cyan,
+                      ),
+                      Text(
+                        "Rabu",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontFamily: 'Montserrat',
+                        ),
+                      ),
+                    ]),
+              ),
+            ),
+          ),
+          Card(
+            margin: const EdgeInsets.all(8),
+            child: InkWell(
+              onTap: () {},
+              splashColor: Colors.black,
+              child: const Center(
+                child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Icon(
+                        Icons.school,
+                        size: 70,
+                        color: Colors.cyan,
+                      ),
+                      Text(
+                        "Kamis",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontFamily: 'Montserrat',
+                        ),
+                      ),
+                    ]),
+              ),
+            ),
+          ),
+          Card(
+            margin: const EdgeInsets.all(8),
+            child: InkWell(
+              onTap: () {},
+              splashColor: Colors.black,
+              child: const Center(
+                child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Icon(
+                        Icons.school,
+                        size: 70,
+                        color: Colors.cyan,
+                      ),
+                      Text(
+                        "Jumat",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontFamily: 'Montserrat',
+                        ),
+                      ),
+                    ]),
+              ),
+            ),
+          ),
+          Card(
+            margin: const EdgeInsets.all(8),
+            child: InkWell(
+              onTap: () {
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => const Carousel()));
+              },
+              splashColor: Colors.black,
+              child: const Center(
+                child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Icon(
+                        Icons.school,
+                        size: 70,
+                        color: Colors.cyan,
+                      ),
+                      Text(
+                        "Carousel",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontFamily: 'Montserrat',
+                        ),
+                      ),
+                    ]),
+              ),
+            ),
+          ),
+          Card(
+            margin: const EdgeInsets.all(8),
+            child: InkWell(
+              onTap: () {
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => const WebViewScreen()));
+              },
+              splashColor: Colors.black,
+              child: const Center(
+                child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Icon(
+                        Icons.school,
+                        size: 70,
+                        color: Colors.cyan,
+                      ),
+                      Text(
+                        "WebView",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontFamily: 'Montserrat',
+                        ),
+                      ),
+                    ]),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
